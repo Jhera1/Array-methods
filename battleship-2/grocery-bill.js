5eb4834e945e2e5cb75aab156ed7eb1094179c13
Justin Armstrong <armstrongjustin034@gmail.com>
2025-01-02 08:42:01 -0800

    reverseVowels

--- battleship-2/grocery-bill.js
+++ battleship-2/grocery-bill.js
@@ -8,8 +8,8 @@
 // const add = (a, b) => a + b
 // console.log(add(78.50, 6))
 
-const sum = arr => arr.reduce((a, b) => a + b)
-console.log(sum([1,2,3]))
+// const sum = arr => arr.reduce((a, b) => a + b)
+// console.log(sum([1,2,3]))
 
 const foodList = [
     {name: 'berries', price: 13.08},
@@ -20,24 +20,37 @@ const foodList = [
     {name: 'salsa', price: 13.09}
 ]
 
-const prices = foodList.map(food => food.price)
-console.log(prices)
+console.log('object keys: ', Object.keys(foodList[0]))
+console.log('object values: ', Object.values(foodList[0]))
+console.log('object entries: ', Object.entries(foodList[0]))
+console.log('Has own property, price: ', foodList[0].hasOwnProperty('price'))
 
-const sumOfPrices = sum(prices)
-console.log(sumOfPrices)
 
-let coupon = .25
-const couponDiscount = sumOfPrices * coupon
+foodList.couponFor = function () {
+    console.log(`Today we have ${foodList[0].name} on sale for just ${(foodList[0].price - 3)} `)
+}
+foodList.couponFor()
 
-const priceAfterDiscount = +(sumOfPrices - couponDiscount).toFixed(2)
-console.log(priceAfterDiscount)
 
-const taxPercent = 7.75 /100
 
-const tax = priceAfterDiscount * taxPercent 
 
-const totalPrice = priceAfterDiscount + tax 
 
-console.log(Number(totalPrice.toFixed(2)))
+
+// const sumOfPrices = sum(prices)
+// console.log(sumOfPrices)
+
+// let coupon = .25
+// const couponDiscount = sumOfPrices * coupon
+
+// const priceAfterDiscount = +(sumOfPrices - couponDiscount).toFixed(2)
+// console.log(priceAfterDiscount)
+
+// const taxPercent = 7.75 /100
+
+// const tax = priceAfterDiscount * taxPercent 
+
+// const totalPrice = priceAfterDiscount + tax 
+
+// console.log(Number(totalPrice.toFixed(2)))
 
 
--- battleship-2/stoplight.js
+++ battleship-2/stoplight.js
@@ -1,5 +1,90 @@
-let currentLightColor = 'Awesome'
+// let currentLightColor = ['red'].random()
+// let lastWords = ''
 
-const printSentence = () => `the light is ${currentLightColor}`
-console.log(printSentence())
+// const remainderOfSentence = () => {
+//     if (currentLightColor == 'green') {
+//         lastWords = 'You can go!'
+//     } else if (currentLightColor == 'red') {
+//         lastWords = 'STOP!!!'
+//     } else if (currentLightColor == 'yellow') {
+//         lastWords = 'Slow Down'
+//     } else {
+//         lastWords = 'Just floor it'
+//     }
+// } 
+// remainderOfSentence()
 
+// const printSentence = () => `the light is ${currentLightColor}, ${lastWords}`
+
+// console.log(printSentence())
+
+
+
+// const whatColorIsTheLight = () => {
+//     let currentLightColor = 'green'
+
+//     if (currentLightColor == 'green') {
+//         console.log('the light is green')
+//     } else if (currentLightColor == 'red') {green
+//         console.log('the light is red')
+//     } else if (currentLightColor == 'yellow') {
+//         console.log('the light is yellow')
+//     } else {
+//         console.log('there is no light')
+//     }
+// }
+// whatColorIsTheLight()
+
+// let currentLightColor = 'red'
+
+
+// const colorOfLight = () => currentLightColor == 'green' ? 'the light is green' :
+//                      currentLightColor == 'red' ? 'the light is red' :
+//                      currentLightColor == 'yellow' ? 'the light is yellow' : 'the light is non operational' 
+//              console.log(colorOfLight())                            
+
+///////////////////ready to skate\\\\\\\\\\\\\\\\\\\\\
+
+// let readyToSkate = false;
+// let energyLevel = 18000;
+// let crewStatus = true;
+// let cameraBattery = "green";
+
+// if (energyLevel >= 24000) {
+//    console.log("Energy level is a go!");
+//    readyToSkate = true;
+// } else {
+//    console.log("WARNING: Insufficient energy!");
+//    readyToSkate = false;
+// }
+
+// if (crewStatus && cameraBattery === "green"){
+//    console.log("Crew & cameraBattery is a go!");
+//    readyToSkate = true;
+// } else {
+//    console.log("WARNING: Crew or cameraBattery not ready!");
+//    readyToSkate = false;
+// }
+
+// if (readyToSkate) {
+//    console.log("Let's goooooo!");
+//    console.log("Time to skate!");
+// } else {
+//    console.log("No skating today.");
+// }
+
+const reverseVowels = (s) => {
+       const vowels = 'aeiouAEIOU'
+       const vowelsInS = s.split('').filter(char => vowels.includes(char))
+       return s.split('').map(char => vowels.includes(char) ? vowelsInS.pop() : char).join('')
+
+};
+
+console.log(reverseVowels('Somewhere In Time'))
+
+
+
+
+
+
+// const reverseVowels = str =>
