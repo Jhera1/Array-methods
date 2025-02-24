546c057926d1880932ccb62e9b644f4f55d6844c
Justin Armstrong <armstrongjustin034@gmail.com>
2025-02-23 22:46:06 -0800

    MWA #2 charThatAppearsOnlyOnce

--- interview/more-weekly-algo.js
+++ interview/more-weekly-algo.js
@@ -2,7 +2,7 @@
 //   an object with the counts of vowels and consonants.
 
 const vowelsConsonantsCount = str => {
-    let counts = {'vowels': 0, 'consonants': 0}
+    const counts = {'vowels': 0, 'consonants': 0}
     const cleanStr = str.replace(/[^A-Za-z]/g, '')
     const vowels = 'AEIOUaeiou'
     for (let char of cleanStr) {
@@ -13,3 +13,24 @@ const vowelsConsonantsCount = str => {
     return counts
 }
 console.log(vowelsConsonantsCount('Somewhere in time'))
+
+
+
+
+// 2.Given a non-empty array of integers where every 
+// element appears twice except for one, find that 
+// single one.
+
+const charThatAppearsOnlyOnce = arr => {
+    const countObj = {}
+    for (let char of arr) {
+        countObj[char] = (countObj[char] || 0) + 1
+    }
+    for (let char in countObj) {
+        if (countObj[char] === 1) {
+            return `${char} appears only once in this array`
+        }     
+    }
+    return `There is no number in ${arr} that appears only once`
+}
+console.log(charThatAppearsOnlyOnce([9, 9, 4, 4, 5, 6, 5, 6]))
