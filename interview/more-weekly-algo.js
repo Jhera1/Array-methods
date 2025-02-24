f7e9b2273e9d452f79e921b116b14f14dca9e0aa
Justin Armstrong <armstrongjustin034@gmail.com>
2025-02-23 22:07:36 -0800

    MWA #1 line 15

--- interview/more-weekly-algo.js
+++ interview/more-weekly-algo.js
@@ -1,1 +1,15 @@
-// Implement a function that takes a string and returns an object with the counts of vowels and consonants.
+// 1.Implement a function that takes a string and returns 
+//   an object with the counts of vowels and consonants.
+
+const vowelsConsonantsCount = str => {
+    let counts = {'vowels': 0, 'consonants': 0}
+    const cleanStr = str.replace(/[^A-Za-z]/g, '')
+    const vowels = 'AEIOUaeiou'
+    for (let char of cleanStr) {
+        if (vowels.includes(char)) {
+            counts.vowels++
+        } else counts.consonants++
+    }
+    return counts
+}
+console.log(vowelsConsonantsCount('Somewhere in time'))
