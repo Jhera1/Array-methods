d083fa0a514d9a7c2cf713d799dde3bba3f694e5
Justin Armstrong <armstrongjustin034@gmail.com>
2025-03-03 09:38:55 -0800

    can place flowers practice

---
+++ interview/scratch.js
@@ -0,0 +1,26 @@
+
+// const canPlaceFlowers = (flowerbed, n) => {
+//     let plantedFlowers = 0
+//     for (let i = 0; i < flowerbed.length; i++) {
+//         if ((i === 0 || flowerbed[i - 1] === 0) &&
+//         (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)) {
+//             flowerbed[i] = 1
+//             plantedFlowers++
+//             i++
+//         }
+//         return plantedFlowers >= n
+//     }
+// };
+
+
+
+
+const canPlaceFlowers = (flowerbed, n) => {
+    const flowersPlaced = 0
+    for (let i = 0; i < flowerbed.length; i++) {
+        while (flowersPlaced <= n) {
+        if (flowerbed[0] )
+        }
+    }
+};
+console.log(canPlaceFlowers())
