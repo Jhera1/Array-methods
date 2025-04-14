b7a8ddc8107a9f5a87224125fea3ae026bc7da50
Justin Armstrong <armstrongjustin034@gmail.com>
2025-04-14 14:37:57 -0700

    added more practice ended on line 309

--- interview/scratch.js
+++ interview/scratch.js
@@ -191,16 +191,16 @@
 //     return resultArr
 // }
 
-const sumExceptSelf = arr => { 
-    let resultArr = []
-    for (let i = 0; i < arr.length; i++) {
-        let newArr = arr.slice(0, i).concat(arr.slice(i + 1)); 
-        let newArrSum = newArr.reduce((a, b) => a * b) 
-        resultArr.push(newArrSum)
-    }
-    return resultArr
-}
-console.log(sumExceptSelf([1, 2, 3, 4, 5])) // Expected new arr [120, 60, 40, 30, 24]
+// const sumExceptSelf = arr => { 
+//     let resultArr = []
+//     for (let i = 0; i < arr.length; i++) {
+//         let newArr = arr.slice(0, i).concat(arr.slice(i + 1)); 
+//         let newArrSum = newArr.reduce((a, b) => a * b) 
+//         resultArr.push(newArrSum)
+//     }
+//     return resultArr
+// }
+// console.log(sumExceptSelf([1, 2, 3, 4, 5])) // Expected new arr [120, 60, 40, 30, 24]
 
 
 
@@ -208,8 +208,107 @@ console.log(sumExceptSelf([1, 2, 3, 4, 5])) // Expected new arr [120, 60, 40, 30
 // Write a function that finds the contiguous subarray with the largest sum and returns that sum.
 // Extension: Implement both the O(n) Kadane's algorithm and the divide-and-conquer approach.
 
+
+
 // Write a function that takes a string as input and returns the string with only the first letter of each word capitalized.
 // Extension : Modify the function to preserve the case of the remaining letters in each word.
 
+// const capitalizeFirstLetter = str => {
+//     const wordArr = str.split(' ')
+    
+//     let resultArr = []
+//     for (let word of wordArr) {
+//        resultArr.push(word[0].toUpperCase() + word.slice(1))   
+//     }
+//    return resultArr.join(' ')
+// }
+// console.log(capitalizeFirstLetter('somewhere in time'))
+
+
 // Given a positive integer n, return it reversed without converting it to a string.
 // Bonus: modify this function to return a boolean expressing if the number is a palindrome.
+
+
+// const reverseInt = n => {
+//     let revNum = 0
+//     while (n > 0) {
+//         let digit = n % 10
+//         console.log(digit)
+//         revNum = revNum * 10 + digit
+//         n = Math.floor(n / 10)
+//         console.log(n)
+//     }
+//     console.log('break')
+//     return revNum
+// }
+// console.log(reverseInt(12345)) // Expected output: 54321
+// console.log(reverseInt(54321)) // Expected output: 12345    
+// console.log(reverseInt(37268)) // Expected output: 86273 
+
+// *********
+// zipWith ( or zip_with ) takes a function and two arrays and 
+// zips the arrays together, applying the function to every pair of values.
+// The function value is one new array.
+
+// If the arrays are of unequal length, the output will only be as 
+// long as the shorter one.
+// (Values of the longer array are simply not used.)
+
+// const zipWith = (fn, a, b) => {
+//     const minLength = Math.min(a.length, b.length);
+//     const result = [];
+
+//     for (let i = 0; i < minLength; i++) {
+//         result.push(fn(a[i], b[i]));
+//     }
+
+//     return result;
+// };
+// console.log(zipWith((x, y) => x * y, [1, 2, 3], [4, 5, 6])); // [4, 10, 18]
+// console.log(zipWith(Math.max, [1, 7, 3], [4, 2]));           // [4, 7]
+
+
+//***************
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// const findUniqNum = arr => {
+//     let common
+//     if (arr[0] === arr[1] || arr[0] === arr[2]) {
+//     common = arr[0]
+//     } else {
+//     common = arr[1]
+//     }
+//     for (i = 0; i < arr.length; i++) {
+//         if (arr[i] !== common) {
+//             return arr[i]
+//         }
+//     }
+// }
+
+// console.log(findUniqNum([ 1, 1, 1, 2, 1, 1 ])) // 2
+// console.log(findUniqNum([ 0, 0, 0.55, 0, 0 ])) // 0.55
+
+// const nums = [1, 2, 3, 4, 5]
+
+// nums.forEach
+
+// ******************
+
+// You will be given an array which lists the current inventory 
+// of stock in your store and another array which lists 
+// the new inventory being delivered to your store today.
+
+// Your task is to write a function that returns the updated 
+// list of your current inventory in alphabetical order.
+
+// Example
+// currentStock = [[25, 'HTC'], [1000, 'Nokia'], [50, 'Samsung'], [33, 'Sony'], [10, 'Apple']]
+// newStock = [[5, 'LG'], [10, 'Sony'], [4, 'Samsung'], [5, 'Apple']]
+
+// updateInventory(currentStock, newStock)  ==>
+// [[15, 'Apple'], [25, 'HTC'], [5, 'LG'], [1000, 'Nokia'], [54, 'Samsung'], [43, 'Son
+
+function updateInventory(curStock, newStock) {
+    // thank you for solving my kata :)
+      return [];
+  }
