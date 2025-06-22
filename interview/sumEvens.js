1dfe068bf7d3e65be3d3d64ad7a8c99e3a9de181
Justin Armstrong <armstrongjustin034@gmail.com>
2025-06-21 20:42:32 -0700

    completed sumEvens.js

--- interview/sumEvens.js
+++ interview/sumEvens.js
@@ -1,23 +1,23 @@
 // Prompt: Write a function that takes a list of numbers and returns the sum of all even numbers in the list.
 // Extension: Modify the function to return both the sum of even numbers and the sum of odd numbers.
 
-// const sumOfEvenAndOddNums = arr => { 
-//     let evenArr = []
-//     let oddArr = []
-//     for (let i = 0; i < arr.length; i++) {
-//         if (arr[i] % 2 === 0) {
-//           evenArr.push(arr[i])
-//         }
 
-//         if (arr[i] % 3 === 0) {
-//             oddArr.push(arr[i])
-//           }
-//      }
-//      return {
-//         'Sum of Even': evenArr.reduce((a, b) => a + b),
-//         'Sum of Odd': oddArr.reduce((a, b) => a + b)
-//      }
-// }
 
-const sumOfEvenAndOddNums = arr => 
+const sumOfEvenAndOddNums = arr => {
+    let oddSum = 0
+    let evenSum = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            evenSum += arr[i]
+        } else {
+            oddSum += arr[i]
+        }
+    }
+    return {
+        'odd sum': oddSum,
+        'even sum': evenSum
+    }
+}
+
+
 console.log(sumOfEvenAndOddNums([1, 2, 3, 4, 5, 6]))
