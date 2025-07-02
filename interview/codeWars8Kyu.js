af97dba0025204ffe292bc361390be0c2844a85a
Justin Armstrong <armstrongjustin034@gmail.com>
2025-07-01 19:24:44 -0700

    completed #5

--- interview/codeWars8Kyu.js
+++ interview/codeWars8Kyu.js
@@ -53,16 +53,16 @@
 // [] --> []
 
 
-const inverseOfNums = nums => {
+// const inverseOfNums = nums => {
   
-  for (let i=0;i<nums.length;i++) {
-    if (nums[i] < 0) return Math.abs(nums[i])
-  }
-  return nums[i] * -1
-}
+//   for (let i=0;i<nums.length;i++) {
+//     if (nums[i] < 0) return Math.abs(nums[i])
+//   }
+//   return nums[i] * -1
+// }
  
-console.log(inverseOfNums([1, 2, 3, 4, 5]))
-console.log(inverseOfNums([-1, -2, -3, -4, -5]))
+// console.log(inverseOfNums([1, 2, 3, 4, 5]))
+// console.log(inverseOfNums([-1, -2, -3, -4, -5]))
 
 
 
@@ -71,3 +71,39 @@ console.log(inverseOfNums([-1, -2, -3, -4, -5]))
 // [1, 2, 3, 4, 5] --> [-1, -2, -3, -4, -5]
 // [1, -2, 3, -4, 5] --> [-1, 2, -3, 4, -5]
 // [] --> []
+
+
+// #5
+// 8 kyu
+// Correct the mistakes of the character recognition software
+// 
+
+// Description:
+// Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+// When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+// Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+// S is misinterpreted as 5
+// O is misinterpreted as 0
+// I is misinterpreted as 1
+// The test cases contain numbers only by mistake.
+
+// FundamentalsStrings
+
+const correctTranslation = str => 
+  str.split('').map(a => 
+    a === '5' ? 'S' :
+    a === '0' ? 'O' :
+    a === '1' ? 'I' :
+    a).join('')
+  
+
+
+
+    console.log(correctTranslation("L0ND0N")); // ,"LONDON"
+    console.log(correctTranslation("DUBL1N")); // "DUBLIN"
+    console.log(correctTranslation("51NGAP0RE")); // "SINGAPORE"
+    console.log(correctTranslation("BUDAPE5T")); // "BUDAPEST"
+    console.log(correctTranslation("PAR15")); // "PARIS"
